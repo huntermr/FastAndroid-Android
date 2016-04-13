f4d6f3db5520dee70510cac2cb69a92d0784f2ab
hunter <hunter_android@163.com>
2016-04-13 16:28:14 +0800

    add vancl layout

---
+++ app/src/main/java/com/hunter/fastandroid/base/BaseFragment.java
@@ -0,0 +1,134 @@
+package com.hunter.fastandroid.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+
+public abstract class BaseFragment extends Fragment implements IBaseView {
+    private BaseActivity mActivity;
+    private View mLayoutView;
+
+    /**
+     * 初始化布局
+     */
+    public abstract int getLayoutRes();
+
+    /**
+     * 初始化视图
+     */
+    public abstract void initView();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mLayoutView = getCreateView(inflater, container);
+        ButterKnife.bind(this, mLayoutView);
+        initView();
+        return mLayoutView;
+    }
+
+    /**
+     * 获取Fragment布局文件的View
+     *
+     * @param inflater
+     * @param container
+     * @return
+     */
+    private View getCreateView(LayoutInflater inflater, ViewGroup container) {
+        return inflater.inflate(getLayoutRes(), container, false);
+    }
+
+    /**
+     * 获取当前Fragment状态
+     *
+     * @return true为正常 false为未加载或正在删除
+     */
+    private boolean getStatus() {
+        return (isAdded() && !isRemoving());
+    }
+
+    /**
+     * 获取Activity
+     *
+     * @return
+     */
+    public BaseActivity getBaseActivity() {
+        if (mActivity == null) {
+            mActivity = (BaseActivity) getActivity();
+        }
+        return mActivity;
+    }
+
+    @Override
+    public void showProgress(boolean flag, String message) {
+        if (getStatus()) {
+            BaseActivity activity = getBaseActivity();
+            if (activity != null) {
+                activity.showProgress(flag, message);
+            }
+        }
+    }
+
+    @Override
+    public void showProgress(String message) {
+        showProgress(true, message);
+    }
+
+    @Override
+    public void showProgress() {
+        showProgress(true);
+    }
+
+    @Override
+    public void showProgress(boolean flag) {
+        showProgress(flag, "");
+    }
+
+    @Override
+    public void hideProgress() {
+        if (getStatus()) {
+            BaseActivity activity = getBaseActivity();
+            if (activity != null) {
+                activity.hideProgress();
+            }
+        }
+    }
+
+    @Override
+    public void showToast(int resId) {
+        if (getStatus()) {
+            BaseActivity activity = getBaseActivity();
+            if (activity != null) {
+                activity.showToast(resId);
+            }
+        }
+    }
+
+    @Override
+    public void showToast(String msg) {
+        if (getStatus()) {
+            BaseActivity activity = getBaseActivity();
+            if (activity != null) {
+                activity.showToast(msg);
+            }
+        }
+    }
+
+    @Override
+    public Context getContext() {
+        return getActivity();
+    }
+
+    @Override
+    public void close() {
+    }
+}
---
+++ app/src/main/java/com/hunter/fastandroid/base/BaseRequestHeader.java
@@ -0,0 +1,10 @@
+package com.hunter.fastandroid.base;
+
+/**
+ * 公共请求头
+ *
+ * @author user
+ */
+public class BaseRequestHeader extends BaseRequest {
+
+}
